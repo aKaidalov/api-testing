c3325506c13c7f993f961f018db108aeca040f92
German Mumma <german.mumma@merada.ee>
2024-10-03 13:34:55 +0300

    feat: initial raw tests

---
+++ src/test/java/api_testing/e2e/DeleteBookingTests.java
@@ -0,0 +1,15 @@
+package api_testing.e2e;
+
+import org.junit.jupiter.api.Test;
+
+public class DeleteBookingTests {
+    @Test
+    public void whenDeleteBooking_thenShouldReturnStatus201() {
+
+    }
+
+    @Test
+    public void givenDeletedBookingId_whenGetBookingById_thenShouldReturnStatus404() {
+
+    }
+}
---
+++ src/test/java/api_testing/smoke/SmokeTests.java
@@ -0,0 +1,72 @@
+package api_testing.smoke;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+
+public class SmokeTests {
+    public static final String API_URL = "https://restful-booker.herokuapp.com/booking";
+
+    @Test
+    public void whenGetBookingIdsIsCalled_thenReturnHttp200() {
+        given()
+                .when()
+                .get(API_URL)
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void whenGetBookingByIdIsCalled_thenReturnHttp200() {
+        String payload = "{\n" +
+                "\"firstname\": " + "\"German\",\n" +
+                "\"lastname\": " + "\"Mumma\",\n" +
+                "\"totalprice\": " + "100\n," +
+                "\"depositpaid\": " + "true\n," +
+                "\"bookingdates\": {\n" +
+                "\"checkin\": " + "\"2023-03-23\",\n" +
+                "\"checkout\": " + "\"2023-03-25\"\n" +
+                "}\n" +
+                "}";
+
+        int bookingId = given()
+                .contentType(JSON.toString())
+                .accept(JSON.toString())
+                .body(payload)
+                .when()
+                .post(API_URL)
+                .then()
+                .extract()
+                .path("bookingid");
+
+        given()
+                .when()
+                .get(API_URL + "/" + bookingId)
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void givenBookingRequest_whenCreateBookingIsCalled_thenReturnHttp200() {
+        String payload = "{\n" +
+                "\"firstname\": " + "\"German\",\n" +
+                "\"lastname\": " + "\"Mumma\",\n" +
+                "\"totalprice\": " + "100\n," +
+                "\"depositpaid\": " + "true\n," +
+                "\"bookingdates\": {\n" +
+                "\"checkin\": " + "\"2023-03-23\",\n" +
+                "\"checkout\": " + "\"2023-03-25\"\n" +
+                "}\n" +
+                "}";
+
+        given()
+                .contentType(JSON.toString())
+                .accept(JSON.toString())
+                .body(payload)
+                .when()
+                .post(API_URL)
+                .then()
+                .statusCode(200);
+    }
+}
