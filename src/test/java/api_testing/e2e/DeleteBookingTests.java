3346623c1809ab06a7bc6ae3a96e8a4706c2f7e3
German Mumma <german.mumma@merada.ee>
2024-10-03 15:26:30 +0300

    feat: delete booking returns 201

--- src/test/java/api_testing/e2e/DeleteBookingTests.java
+++ src/test/java/api_testing/e2e/DeleteBookingTests.java
@@ -1,11 +1,33 @@
 package api_testing.e2e;
 
+import api_testing.controller.BookingApi;
+import api_testing.dto.Booking;
+import api_testing.dto.BookingResponse;
+import api_testing.generator.BookingGenerator;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DeleteBookingTests {
+
+    private static BookingApi bookingApi;
+
+    @BeforeAll
+    public static void setup() {
+        bookingApi = new BookingApi();
+    }
+
     @Test
     public void whenDeleteBooking_thenShouldReturnStatus201() {
+        Booking booking = BookingGenerator.getFullPayload();
 
+        BookingResponse bookingResponse = bookingApi.createBooking(booking).as(BookingResponse.class);
+        int bookingId = bookingResponse.getBookingid();
+        Response deleteBookingResponse = bookingApi.deleteBooking(bookingId);
+
+        assertThat(deleteBookingResponse.getStatusCode()).isEqualTo(201);
     }
 
     @Test
