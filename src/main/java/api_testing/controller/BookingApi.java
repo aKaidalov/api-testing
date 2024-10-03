0ab0a2a5c40b6b2e575dfc4cfa61ead2bc8a8197
German Mumma <german.mumma@merada.ee>
2024-10-03 14:40:29 +0300

    feat: add dto with create booking method

--- src/main/java/api_testing/controller/BookingApi.java
+++ src/main/java/api_testing/controller/BookingApi.java
@@ -1,8 +1,10 @@
 package api_testing.controller;
 
+import api_testing.dto.Booking;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 
 public class BookingApi {
     public static final String API_URL = "https://restful-booker.herokuapp.com/booking";
@@ -13,6 +15,19 @@ public class BookingApi {
                 .when()
                 .get(API_URL)
                 .then()
+                .extract().response();
+    }
+
+    public Response createBooking(Booking booking) {
+        return given()
+                .contentType(JSON.toString())
+                .accept(JSON.toString())
+                .body(booking)
+                .log().body()
+                .when()
+                .post(API_URL)
+                .then()
+                .log().body()
                 .extract().response();
     }
 }
