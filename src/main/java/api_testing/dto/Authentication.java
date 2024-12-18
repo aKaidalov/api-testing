512101185af0e510b865902f1258edbc5df660b9
Aleksandr Kaidalov <alkaid@ttu.ee>
2024-12-18 21:19:57 +0200

    test-1: update Authentication class with auto constructor creation

--- src/main/java/api_testing/dto/Authentication.java
+++ src/main/java/api_testing/dto/Authentication.java
@@ -1,8 +1,12 @@
 package api_testing.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Authentication {
     private String username;
     private String password;
