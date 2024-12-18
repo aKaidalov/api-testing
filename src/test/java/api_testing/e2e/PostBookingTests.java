7c734d87cf65140e6876248e178d2fca8822647a
Aleksandr Kaidalov <alkaid@ttu.ee>
2024-12-18 22:00:32 +0200

    test-4: add test for posting booking with wrong Accept header

---
+++ src/test/java/api_testing/e2e/PostBookingTests.java
@@ -0,0 +1,32 @@
+package api_testing.e2e;
+
+import api_testing.controller.BookingApi;
+import api_testing.dto.Booking;
+import api_testing.generator.BookingGenerator;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.given;
+
+public class PostBookingTests {
+
+    private BookingApi bookingApi = new BookingApi();
+
+    @BeforeEach
+    public void initializeBookingApi() {
+        bookingApi = new BookingApi();
+    }
+
+    @Test
+    public void whenPostBookingWithWrongAcceptHeader_thenReturnStatus418() {
+        Booking generatedBooking = BookingGenerator.getFullPayload();
+
+        Response response = bookingApi.createBooking(generatedBooking, ContentType.HTML);
+
+        assertThat(response.getStatusCode()).isEqualTo(418);
+    }
+}
