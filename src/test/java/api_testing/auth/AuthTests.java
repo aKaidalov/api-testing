17712098c5adf08b790b2e244ce44bdc38c6fefe
Aleksandr Kaidalov <alkaid@ttu.ee>
2024-12-18 21:28:08 +0200

    test-3: add test for authentication returns token

--- src/test/java/api_testing/auth/AuthTests.java
+++ src/test/java/api_testing/auth/AuthTests.java
@@ -19,4 +19,10 @@ public class AuthTests {
         Response response = AuthApi.login("wrongUser", "wrongPassword");
         assertThat(response.jsonPath().getString("reason")).isEqualTo("Bad credentials");
     }
+
+    @Test
+    public void whenPostAuthentication_thenReturnToken() {
+        String token = AuthApi.getToken();
+        assertThat(token).isNotNull();
+    }
 }
